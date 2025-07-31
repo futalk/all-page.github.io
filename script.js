f9b9e6dba0279f3f2dc80ab1126e6846c3039536
futalk <51226488+futalk@users.noreply.github.com>
2025-07-31 17:38:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("主站已加载：https://your-username.github.io");
-  console.log("技术输出 · 网络安全 · 知识管理");
+  console.log("主站已加载：https://putdown.github.io");
+  console.log("技术输出 · 网络安全 · 知识管理 by Putdown");
 });
