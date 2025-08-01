4a5e998809c50a46f1d0457af664e61929e7362b
futalk <51226488+futalk@users.noreply.github.com>
2025-08-01 17:51:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+// script.js（可选分离）
 document.addEventListener("DOMContentLoaded", () => {
   console.log("主站已加载：https://putdown.github.io");
-  console.log("分类展示：内容输出 × 工具服务 | by Putdown");
+  console.log("功能：暗色模式 | Base64 工具 | 匿名统计 | by Putdown");
 });
